af7c2abf8a52653f59869b8a3d83d46f62b1d6bc
Krasser Tamás <krasser.tamas@gmail.com>
2019-09-26 19:57:06 +0200

    style updates

--- src/utils/htmlSerialize.jsx
+++ src/utils/htmlSerialize.jsx
@@ -46,49 +46,70 @@ const htmlSerialize = (type, element, content, children, index) => {
     case 'heading1': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'h1', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'h1', gutterBottom: true, className: 'richtext h h1' },
+          index,
+        ),
         children,
       )
     }
     case 'heading2': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'h2', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'h2', gutterBottom: true, className: 'richtext h h2' },
+          index,
+        ),
         children,
       )
     }
     case 'heading3': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'h3', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'h3', gutterBottom: true, className: 'richtext h h3' },
+          index,
+        ),
         children,
       )
     }
     case 'heading4': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'h4', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'h4', gutterBottom: true, className: 'richtext h h4' },
+          index,
+        ),
         children,
       )
     }
     case 'heading5': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'h5', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'h5', gutterBottom: true, className: 'richtext h h5' },
+          index,
+        ),
         children,
       )
     }
     case 'heading6': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'h6', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'h6', gutterBottom: true, className: 'richtext h h6' },
+          index,
+        ),
         children,
       )
     }
     case 'paragraph': {
       return React.createElement(
         Typography,
-        propsWithUniqueKey({ variant: 'body1', gutterBottom: true }, index),
+        propsWithUniqueKey(
+          { variant: 'body1', gutterBottom: true, className: 'richtext p' },
+          index,
+        ),
         children,
       )
     }
